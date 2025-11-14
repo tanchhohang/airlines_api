3e15bcb64fb842a018bf998eb1f3eb9bca1c8f97
Yuko <tanchho.limbu25@gmail.com>
2025-11-14 17:09:01 +0545

    feat: Add User, Passenger and Booking serializers

--- airlines_api/bookings/serializer.py
+++ airlines_api/bookings/serializer.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
-from .models import Sector, Airline
+from .models import *
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = [
+            'user_id',
+            'api_password',
+            'agency_id'
+        ]
 
 class SectorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +19,14 @@ class AirlineSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Airline
         fields = '__all__'
+
+class PassengerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Passenger
+        fields = '__all__'
+
+class BookingSeriazlizer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
